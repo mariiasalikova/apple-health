5a4cf1557d291e598750553e51ce67381a55f52e
Саликова Мария Тимуровна <salikova.work@gmail.com>
2024-05-10 15:00:40 +0300

    steps added. title added.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import re
 from lxml import etree
 
-
+st.title(':rainbow-background[Apple health analyser] :heart:')
 #OTHER_KEYS = ["type", "sourceName", "unit"]
 ALL_KEYS = ["startDate", "value", "type"]
 uploaded_file = st.file_uploader("Choose a file")
@@ -13,7 +13,8 @@ if uploaded_file is not None:
     root = tree.getroot()
     records = tree.xpath("//Record")
     # Get all records where as source the apple watch is
-    df = pd.DataFrame([{key: r.get(key) for key in ALL_KEYS} for r in records if 'Apple' in r.attrib['sourceName']])
+    #if 'Apple' in r.attrib['sourceName']
+    df = pd.DataFrame([{key: r.get(key) for key in ALL_KEYS} for r in records])
 
     df_heart = df.query('type == "HKQuantityTypeIdentifierHeartRate"')
     
@@ -21,3 +22,9 @@ if uploaded_file is not None:
     df_heart['value'] = pd.to_numeric(df_heart['value'])
     
     st.line_chart(df_heart, x="startDate", y="value")
+    
+    df_steps = df.query('type == "HKQuantityTypeIdentifierStepCount"')
+    df_steps['value'] = df_steps['value'].str.replace(',', '.')
+    df_steps['value'] = pd.to_numeric(df_steps['value'])
+    
+    st.line_chart(df_steps, x="startDate", y="value")
